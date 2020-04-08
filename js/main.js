1fc6d4d47e947dcdca6dbff563d0522440f32af7
Adolfo <adolfo_zuazo@hotmail.com>
2020-04-07 22:18:21 -0300

    20200407 se agrego proyecto del clima

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const btnHablemos = document.getElementById('hablemos')
-const introContenedor = document.getElementById('intro-contenedor')
-
-btnHablemos.addEventListener('click', ()=>{
-    introContenedor.classList.toggle('active')
-})
