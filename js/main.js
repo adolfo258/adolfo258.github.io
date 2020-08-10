7f7ba152873ec8f9da0a758f125237e049aa40a2
Adolfo <adolfo_zuazo@hotmail.com>
2020-08-10 19:49:56 -0300

    Puse mailto en boton de enviar mensaje.

--- js/main.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('btn-mensaje').addEventListener('click', ()=>{
-    document.getElementById('popupMensaje').classList.add('active')
-})
-
-document.getElementById('popup-cerrar').addEventListener('click', ()=>{
-    document.getElementById('popupMensaje').classList.remove('active')
-})
